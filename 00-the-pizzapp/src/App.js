e7723455bb2170c9cb6f67019dc6251ec043f0b8
40detectives <9024463+40detectives@users.noreply.github.com>
2026-01-01 21:59:04 +0100

    feat(props): introduce component props for data flow
    Introduces a reusable `Pizza` component.
    Illustrates one-way data flow by passing data to child components via props to display different pizza details.

--- 00-the-pizzapp/src/App.js
+++ 00-the-pizzapp/src/App.js
@@ -1,8 +1,31 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div",
-    {} /* here goes any attribute for the element like "id", etc */,
-    React.createElement("h1", {}, "Pixel Perfect Pizzas")
+    {
+      /* here goes any attribute for the element like "id", etc */
+    },
+    [
+      React.createElement("h1", {}, "Padre Gino's"),
+      React.createElement(Pizza, {
+        name: "The Pepperoni Pizza",
+        description: "Mozzarella Cheese, Pepperoni",
+      }),
+      React.createElement(Pizza, {
+        name: "The Hawaiian Pizza",
+        description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+      }),
+      React.createElement(Pizza, {
+        name: "The Big Meat Pizza",
+        description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+      }),
+    ]
   );
 };
 
