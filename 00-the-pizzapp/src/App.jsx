150acee21efff1047ccaed87f38800479496597e
40detectives <9024463+40detectives@users.noreply.github.com>
2026-01-23 20:59:36 +0100

    feat(Pizza.jsx): display images for pizzas from API
    Now we can request data to our API server, like the pizza images here.
    
    This enables visual representation for each pizza by introducing an 'image' prop.
    The Pizza component now renders an `<img>` tag, and `App.jsx` passes corresponding image paths for each pizza.

--- 00-the-pizzapp/src/App.jsx
+++ 00-the-pizzapp/src/App.jsx
@@ -8,14 +8,17 @@ export const App = () => {
       <Pizza
         name="Pepperoni Pizza"
         description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
       />
       <Pizza
         name="The Hawaiian Pizza"
         description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
       />
       <Pizza
         name="The Big Meat Pizza"
         description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
       />
     </div>
   );
