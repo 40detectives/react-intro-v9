0d3bc577e3dcd94a1c7eb44969d798e20c5a11cd
40detectives <9024463+40detectives@users.noreply.github.com>
2026-01-28 17:43:36 +0100

    task(custom-hooks): add debug value to pizza of the day hook
    This showcases how to improve DX by displaying a helpful name next to custom hooks in React DevTools, since some custom hooks can have a complex structure that’s harder to inspect.

--- 00-the-pizzapp/src/usePizzaOfTheDay.js
+++ 00-the-pizzapp/src/usePizzaOfTheDay.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useDebugValue, useEffect, useState } from "react";
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  useDebugValue(pizzaOfTheDay ? pizzaOfTheDay.name : "loading...");
 
   useEffect(() => {
     (async function fetchPizzaOfTheDay() {
