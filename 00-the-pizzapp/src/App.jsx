e991db5f48f38421b193ac8936650c61310528aa
40detectives <9024463+40detectives@users.noreply.github.com>
2026-01-28 15:45:53 +0100

    feat(logo): add logo class to app's main heading

--- 00-the-pizzapp/src/App.jsx
+++ 00-the-pizzapp/src/App.jsx
@@ -5,7 +5,7 @@ import Order from "./Order";
 export const App = () => {
   return (
     <div>
-      <h1>Padre Gino's – Order Now</h1>
+      <h1 className="logo">Padre Gino's – Order Now</h1>
       <Order />
     </div>
   );
