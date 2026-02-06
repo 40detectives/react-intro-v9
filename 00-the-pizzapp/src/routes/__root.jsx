b47fde22318ed750beecf40258efb2de31316e80
40detectives <9024463+40detectives@users.noreply.github.com>
2026-02-06 14:47:00 +0100

    feat(query): set up React Query provider
    Sets up the React Query client and provider to enable data fetching and caching capabilities across the application.
    
    Integrates React Query Devtools for improved development and debugging experience.

--- 00-the-pizzapp/src/routes/__root.jsx
+++ 00-the-pizzapp/src/routes/__root.jsx
@@ -1,3 +1,4 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useState } from "react";
@@ -22,6 +23,7 @@ function RootComponent() {
         </div>
       </CartContext.Provider>
       <TanStackRouterDevtools />
+      <ReactQueryDevtools />
     </>
   );
 }
