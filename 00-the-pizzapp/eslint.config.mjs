6646f5239f88b78eee068b6d83393a2fb208b5f9
40detectives <9024463+40detectives@users.noreply.github.com>
2026-02-06 12:50:02 +0100

    chore(query): add TanStack Query, ESLint plugin & bump TanStack Router
    Introduces TanStack React Query for efficient server state management.
    Integrates TanStack Query ESLint plugin to ensure best practices.
    Includes TanStack React Query DevTools for enhanced development experience.
    
    Also bumps minor versions for TanStack Router packages and their devtools/plugins.

--- 00-the-pizzapp/eslint.config.mjs
+++ 00-the-pizzapp/eslint.config.mjs
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 import prettier from "eslint-config-prettier";
-import reactPlugin from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactPlugin from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -19,6 +20,7 @@ export default [
   reactPlugin.configs.flat["jsx-runtime"],
   reactHooks.configs.flat.recommended,
   jsxA11y.flatConfigs.recommended,
+  ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
