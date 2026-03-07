2a2910d6f55b084b83129fb869c9d3065958e686
40detectives <9024463+40detectives@users.noreply.github.com>
2026-03-07 20:00:10 +0100

    test(vitest-browser): add some Vitest browser mode test examples
    This demonstrates how to write browser-mode Vitest tests for React components, including mocking context providers and router setup.

--- 00-the-pizzapp/src/Header.jsx
+++ 00-the-pizzapp/src/Header.jsx
@@ -11,7 +11,10 @@ export default function Header() {
         <h1 className="logo">Padre Gino's Pizza</h1>
       </Link>
       <div className="nav-cart">
-        🛒<span className="nav-cart-number">{cart.length}</span>
+        🛒
+        <span data-testid="cart-number" className="nav-cart-number">
+          {cart.length}
+        </span>
       </div>
     </nav>
   );
