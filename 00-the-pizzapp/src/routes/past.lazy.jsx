f06032b4dc700d63b619c06191f01a615c954eaa
40detectives <9024463+40detectives@users.noreply.github.com>
2026-04-02 10:18:53 +0200

    feat(Suspense): add Suspense and the use hook along TanStack QueryClient
    This change demonstrates how to integrate the React 19 `use` hook and Suspense for handling data fetching with TanStack Query. It involves configuring `experimental_prefetchInRender` on the QueryClient and utilizing the `promise` property from `useQuery` in conjunction with `use()` and `Suspense`.

--- 00-the-pizzapp/src/routes/past.lazy.jsx
+++ 00-the-pizzapp/src/routes/past.lazy.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { useState, Suspense, use } from "react";
 import getPastOrder from "../api/getPastOrder";
 import getPastOrders from "../api/getPastOrders";
 import ErrorBoundary from "../ErrorBoundary";
@@ -12,36 +12,42 @@ export const Route = createLazyFileRoute("/past")({
 });
 
 function ErrorBoundaryWrappedPastOrderRoutes(props) {
+  const [page, setPage] = useState(1);
+  const loadedPromise = useQuery({
+    queryKey: ["past-orders", page],
+    queryFn: () => getPastOrders(page),
+    staleTime: 60000,
+  }).promise;
   return (
     <ErrorBoundary>
-      <PastOrdersRoute {...props} />
+      <Suspense
+        fallback={
+          <div className="past-orders">
+            <h2>Loading Past Orders…</h2>
+          </div>
+        }
+      >
+        <PastOrdersRoute
+          loadedPromise={loadedPromise}
+          page={page}
+          setPage={setPage}
+          {...props}
+        />
+      </Suspense>
     </ErrorBoundary>
   );
 }
 
-function PastOrdersRoute() {
-  const [page, setPage] = useState(1);
-  const [focusedOrder, setFocusedOrder] = useState();
-  const { isLoading, data } = useQuery({
-    queryKey: ["past-orders", page],
-    queryFn: () => getPastOrders(page),
-    staleTime: 60000,
-  });
+function PastOrdersRoute({ page, setPage, loadedPromise }) {
+  const data = use(loadedPromise);
 
+  const [focusedOrder, setFocusedOrder] = useState();
   const { isLoading: isLoadingPastOrder, data: pastOrderData } = useQuery({
     queryKey: ["past-order", focusedOrder],
     queryFn: () => getPastOrder(focusedOrder),
     staleTime: 24 * 60 * 60 * 1000, // one day in milliseconds,
     enabled: !!focusedOrder,
   });
-
-  if (isLoading) {
-    return (
-      <div className="past-orders">
-        <h2>LOADING…</h2>
-      </div>
-    );
-  }
 
   return (
     <div className="post-orders">
